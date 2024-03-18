3194a805b927d7f4e3adc998ad9ae37b0f4882d3
Cream2324 <143768232+Cream2324@users.noreply.github.com>
2024-03-18 09:16:51 +0700

    commit

--- Cream_1101_Scrabble.py
+++ Cream_1101_Scrabble.py
@@ -50,7 +50,13 @@ def get_frequency_dict(sequence):
     return freq
 
 
-def get_word_score(word: str, n: int):
+# (end of helper code)
+# -----------------------------------
+
+#
+# Problem #1
+#
+def get_word_score(word: str, n: int) -> int:
     """
     Returns the score for a word. Assumes the word is a valid word.
 
@@ -65,44 +71,141 @@ def get_word_score(word: str, n: int):
     n: integer (HAND_SIZE; i.e., hand size required for additional points)
     returns: int >= 0
     """
-
-    # checking pre-cont=dition
-    assert isinstance(word, str), "word must be a string"
-
-    if len(word) > 0:
-        word = word.lower()
-        assert word.islower()
-        assert len(word)
-
+    # TO DO ... <-- Remove this comment when you code this function
+    assert isinstance(word,str), "word must be a string"
+    word = word.lower()
+    assert word.islower(), "lower() conversion failed"
+    assert len(word) > 0, "word must not be empty"
     assert isinstance(n, int), "n must be an int"
     assert n > 0, "hand length n must not be 0"
 
+    # magic coding
+    word_score = 1
 
-    #Psuedocode
-
+    # Pseudocode
     """
-    For each letter in word
+    for each letter in word
         get letter score from dict SCRABBLE_LETTER_VALUES
         add up all the letter scores
-
-    multiply by length of words
+    
+    multiply by length of word
     followed by bonus calculation
-    example, if n=7 and you make the word 'waybel' on the first try
-    it would be worth 115 points
+    example if n=7 and you make the word "waybill" on the first try.
+    it would be worth 155 points (the base score for "waybill" is (4++))
     """
-    word_score = 0
+    total_letter_score = 0
+
     for letter in word:
-        word_score += SCRABBLE_LETTER_VALUES[letter]
-    word_score *= len(word)
+        letter_score = SCRABBLE_LETTER_VALUES[letter]
+        total_letter_score += letter_score
+    
+    total_letter_score *= n
+
     if len(word) == n:
-        word_score += 50
+        total_letter_score += 50
 
-    # checking post-condition
-    assert word_score >= 0, "score calculation failed - score is negative"
-    assert isinstance(word_score, int), "score must be integer"
+    print(total_letter_score)
 
+
+    # checking post-conditions
+    assert word_score > 0, "score calculation failed"
+    assert isinstance(word_score, int), "score must be int"
 
     return word_score
 
-# testcase
-print(get_word_score("happy", 7))
+#testcases
+get_word_score("waybill", 7) #155
+
+# Problem #2
+def display_hand(hand):
+    """
+    Displays the letters currently in the hand.
+
+    For example:
+    >>> display_hand({'a':1, 'x':2, 'l':3, 'e':1})
+    Should print out something like:
+       a x x l l l e
+    The order of the letters is unimportant.
+
+    hand: dictionary (string -> int)
+    """
+    for letter in hand.keys():
+        for _ in range(hand[letter]):
+            print(letter, end=" ")       # print all on the same line
+    print()                             # print an empty line
+display_hand({'a':1, 'x':2, 'l':3, 'e':1})
+
+
+
+def deal_hand(n):
+    """
+    Returns a random hand containing n lowercase letters.
+    At least n/3 the letters in the hand should be VOWELS.
+
+    Hands are represented as dictionaries. The keys are
+    letters and the values are the number of times the
+    particular letter is repeated in that hand.
+
+    n: int >= 0
+    returns: dictionary (string -> int)
+    """
+    hand = {}
+    num_vowels = n // 3
+
+    for i in range(num_vowels):
+        x = VOWELS[random.randrange(0, len(VOWELS))]
+        hand[x] = hand.get(x, 0) + 1
+
+    for i in range(num_vowels, n):
+        x = CONSONANTS[random.randrange(0, len(CONSONANTS))]
+        hand[x] = hand.get(x, 0) + 1
+
+    return hand
+
+print(deal_hand(HAND_SIZE))
+
+
+def update_hand(hand: dict, word: str) -> dict:
+    """
+    Assumes that 'hand' has all the letters in word.
+    In other words, this assumes that however many times
+    a letter appears in 'word', 'hand' has at least as
+    many of that letter in it. 
+
+    Updates the hand: uses up the letters in the given word
+    and returns the new hand, without those letters in it.
+
+    Has no side effects: does not modify hand.
+
+    word: string
+    hand: dictionary (string -> int)    
+    returns: dictionary (string -> int)
+    """
+    # TO DO ... <-- Remove this comment when you code this function
+    """
+    make a hand.copy()
+    for every letter in word
+        use the letter has a key to look up in the hand dict
+        and subtract one from the dict values letter counts
+    
+    return handcoppy
+    """
+hand = {'a':1, 'q':1, 'l':2, 'm':1, 'u':1, 'i':1}
+display_hand(hand)
+new_hand = update_hand
+print
+
+
+# Problem 3
+#
+def is_valid_word(word: str, hand: dict, word_list: list) -> bool:
+    """
+    Returns True if word is in the word_list and is entirely
+    composed of letters in the hand. Otherwise, returns False.
+
+    Does not mutate hand or word_list.
+
+    word: string
+    hand: dictionary (string -> int)
+    word_list: list of lowercase strings
+    """
